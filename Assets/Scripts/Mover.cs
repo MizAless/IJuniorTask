60671b70fa2fb66a6fbd899c70de10e04fdb4b32
MizAless <44200635+MizAless@users.noreply.github.com>
2024-05-19 02:07:01 +0700

    Fix mover

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
@@ -13,6 +11,6 @@ public class Mover : MonoBehaviour
 
     private void Move()
     {
-        transform.Translate(transform.forward * _speed * Time.deltaTime);
+        transform.Translate(transform.forward * _speed * Time.deltaTime, Space.World);
     }
 }
